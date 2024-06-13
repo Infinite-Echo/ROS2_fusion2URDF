dc7d7b68c4f4446dc7b74be3dfe1fb732bf73ef8
Infinite-Echo <colin.fuelberth@icloud.com>
2024-06-13 17:42:49 -0500

    added stl exporting, fixed URDF formatting

--- URDF_Exporter_test/urdf/URDF.py
+++ URDF_Exporter_test/urdf/URDF.py
@@ -3,21 +3,31 @@ from .joint import Joint
 from .urdf_utils import get_occurrence_tf, tf_to_rpy_str, tf_to_xyz_str, get_joint_child_occ, parse_occ_name, parse_name
 from xml.etree.ElementTree import ElementTree, Element, SubElement
 import xml.etree.ElementTree as ET
+import xml.dom.minidom
 import numpy as np
 from scipy.spatial.transform.rotation import Rotation as R
 import adsk, adsk.core, adsk.fusion, traceback
+import os, sys
 
 
 class URDF(ElementTree):
-    def __init__(self, robot_name: str, export_path: str):
+    def __init__(self, robot_name: str, export_path: str, app: adsk.core.Application):
         super().__init__(
             element=Element(
                 "robot",
                 {"name": robot_name, "xmlns:xacro": "http://www.ros.org/wiki/xacro"},
             )
         )
+        self.package_name = f'{robot_name}_description'
         self.export_path = export_path
         self.robot_name = robot_name
+        self.app = app
+        design = adsk.fusion.Design.cast(self.app.activeProduct)
+        self.stl_export_manager = design.exportManager
+
+        # Create Xacro constant for package_name
+        self.getroot().append(Element('xacro:property', attrib={'name':'package_name', 'value':self.package_name}))
+        os.makedirs(f'{self.export_path}/{self.package_name}/src/meshes', exist_ok=True)
 
     def create_base_link(self, base_link_occ: adsk.fusion.Occurrence):
         base_link = Link('base')
@@ -37,15 +47,16 @@ class URDF(ElementTree):
         }
         base_link.set_inertial(mass=mass, xyz="0.0 0.0 0.0", inertia_dict=inertia_dict)
         self.append_link(base_link)
+        self.export_stl(base_link_occ)
 
-    def traverse_link(self, app: adsk.core.Application, parent_link: adsk.fusion.Occurrence, parent_joint: adsk.fusion.AsBuiltJoint = None):
+    def traverse_link(self, parent_link: adsk.fusion.Occurrence, parent_joint: adsk.fusion.AsBuiltJoint = None):
         for joint in parent_link.asBuiltJoints:
             if (parent_joint != None) and (parent_joint.name == joint.name):
                 continue
             child_link = get_joint_child_occ(parent=parent_link, joint=joint)
             self.create_link(child_link=child_link, child_joint=joint, parent_link=parent_link, parent_joint=parent_joint)
             if(child_link.asBuiltJoints.count >= 2):
-                self.traverse_link(app=app, parent_link=child_link, parent_joint=joint)
+                self.traverse_link(parent_link=child_link, parent_joint=joint)
             else:
                 return
                 
@@ -99,6 +110,7 @@ class URDF(ElementTree):
         new_link.set_inertial(mass=mass, xyz="0.0 0.0 0.0", inertia_dict=inertia_dict)
         self.append_link(new_link)
         self.append_joint(new_joint)
+        self.export_stl(child_link)
         return
 
     def append_link(self, link: Element):
@@ -107,6 +119,30 @@ class URDF(ElementTree):
     def append_joint(self, joint: Element):
         self.getroot().append(joint)
 
+    def export_stl(self, occ: adsk.fusion.Occurrence):
+        stl_options = self.stl_export_manager.createSTLExportOptions(occ)
+        stl_options.filename = f'{self.export_path}/{self.package_name}/src/meshes/{parse_occ_name(occ=occ)}.stl'
+        self.stl_export_manager.execute(stl_options)
+        del(stl_options)
+
     def export(self):
-        ET.indent(self, "  ")
-        self.write(f'{self.export_path}/{self.robot_name}.xacro')
+        xml_string = ET.tostring(self.getroot(), 'utf-8')
+        formatted_xml = self.prettify_urdf(xml_string=xml_string)
+        with open(f'{self.export_path}/{self.robot_name}.xacro', 'w') as file:
+            file.write(formatted_xml)
+
+    def prettify_urdf(self, xml_string):
+        """Return a pretty-printed XML string for the given URDF."""
+        dom = xml.dom.minidom.parseString(xml_string)
+        pretty_xml = dom.toprettyxml(indent="  ")
+
+        # Add new lines between <link> and <joint> elements for better readability
+        lines = pretty_xml.split('\n')
+        new_lines = []
+        for line in lines:
+            if (line.strip() == '</link>') or (line.strip() == '</joint>') or (line.strip().startswith('<xacro:property')):
+                new_lines.append(f'{line}\n')
+            else:
+                new_lines.append(line)
+        
+        return '\n'.join(new_lines)
